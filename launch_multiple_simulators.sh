25d6185f132a78f0ad11b8725ac190f530f39dd0
Ilias Karim <ilias@weav.io>
2018-11-16 11:53:22 -0500

    Touch up launch_multiple_simulators.sh

--- launch_multiple_simulators.sh
+++ launch_multiple_simulators.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+BUNDLE_IDENTIFIER="example.CustomSimulatorsExample"
+PROJECT_NAME="CustomSimulatorsExample"
+TARGET_NAME="CustomSimulatorsExample"
+
 xcrun simctl shutdown all
 
-path=$(find ~/Library/Developer/Xcode/DerivedData/CustomSimulatorsExample-*/Build/Products/Debug-iphonesimulator -name "CustomSimulatorsExample.app" | head -n 1)
-echo "${path}"
+APP=$(find ~/Library/Developer/Xcode/DerivedData/${PROJECT_NAME}-*/Build/Products/Debug-iphonesimulator -name "${TARGET_NAME}.app" | head -n 1)
+echo "Launching ${APP} in multiple simulators.."
 
 filename=MultiSimConfig.txt
 grep -v '^#' $filename | while read -r line
@@ -11,5 +15,5 @@ do
   echo $line
   xcrun instruments -w "$line"
   xcrun simctl install booted $path
-  xcrun simctl launch booted example.CustomSimulatorsExample
+  xcrun simctl launch booted ${BUNDLE_IDENTIFIER}
 done
